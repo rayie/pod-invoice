a22c650345c3eff7ed041adb2f7288b3c6519c57
root <root@ip-172-31-11-74.us-west-2.compute.internal>
2017-02-20 19:28:45 -0800

    rie, added conversion script

--- qb_export_to_db.js
+++ qb_export_to_db.js
@@ -8,15 +8,16 @@ const _ = require('lodash');
 const u2p = require('url2pdf');
 
 var F = function(){
+	var self = this;
+	this.dbconn = null;
   this.init = function(fn){
-    var self = this;
     var dburl = process.env.WINWIN_CONN;
     console.log(dburl);
     var ccdb_at = dburl.split(/@/).pop();
     console.log("attempting connection to " + ccdb_at);
 		return MC.connect(dburl)
 		.then(function(dbconn){
-      
+     	self.dbconn=dbconn; 
       self[fn](dbconn); 
     })
     .catch(function(err){
@@ -35,7 +36,7 @@ var F = function(){
       colRef["poNumber"] = colRef["p._o."] *1;
       delete colRef["p._o."];
       delete colRef[""];
-      console.log(colRef);
+      //console.log(colRef);
 
       lines = lines.map(function(r){
         return r.split(/\t/g);
@@ -59,21 +60,59 @@ var F = function(){
       return dbconn.collection("winwin_inv").insertMany(lines)
     })
     .then(function(insertResult){
-      console.log(insertResult);
+      //console.log(insertResult);
     })
     .catch(function(err){ console.log(err); })
   }
 
-  this.to_pdf= function(){
-    var url = "http://winwinproducts.com/all/pod_1Z44V1400352015776.htm";
+  this.all_invs_to_pdf = function(dbconn){
 
+		dbconn.collection("winwin_inv").aggregate([
+				{ $match: { rendered: {$ne: true}  } },
+				{ $group: {_id: "$num", n:{$sum:1}} }
+		]).toArray()
+		.then(self._iterate_invs)
+    .catch(function(err){ console.log(err); })
+  }
+
+	this._iterate_invs = function(rr){
+		if ( rr.length === 0 ) return process.exit();
+		var r = rr.pop();
+		console.log(r);
+		return self._to_pdf( r._id ,rr);
+	},
+
+  this._to_pdf= function(num,rr){
+    var url = "http://localhost:3000/inv/" + num;
+		console.log(url);
+    u2p.renderPdf( url, {
+      id: num+"_inv",
+      //saveDir: "/var/www/pod-invoice/data/generatedinv"
+      saveDir: "/var/www/pod-invoice/public/invs"
+    })
+    .then(function(path){
+      //console.log(path);
+			return self.dbconn.collection("winwin_inv").updateOne({
+			},{$set: { rendered:true} })
+    })
+		.then(function(updateResult){
+			console.log(updateResult.result);
+			return self._iterate_invs(rr);
+		})
+    .catch(function(err){ console.log(err); })
+
+  }
+
+
+  this.sample_to_pdf= function(){
+    var url = "http://winwinproducts.com/all/pod_1Z44V1400352015776.htm";
     url = "http://localhost:3000";
     u2p.renderPdf( url, {
       id:"sampleOut"+Math.random().toString(), 
-      saveDir: "/var/www/yung-invoice/data",
+      saveDir: "/var/www/pod-invoice/data/generatedinv",
     })
     .then(function(path){
-      console.log(path);
+      //console.log(path);
       process.exit();
     })
     .catch(function(err){ console.log(err); })
--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,61 @@ const _ = require("lodash");
 var Promise = require("bluebird");
 var read = Promise.promisify( require("fs").readFile );
 
+
+function procInvoice(lines){
+	var hdr = lines[0];
+	hdr.subtotal = lines.reduce(function(t,r){ return t+r.amount; },0)
+	hdr.lines = lines;
+	//console.log('hdr',hdr);
+	return hdr;
+}
+
 function absorb(req){
   return req.db.aggregate([
       { $group: {_id: "$num", n:{$sum:1}} }
   ]).toArray()
   .then(function(rr){
     r = rr.filter( function(r){ return r.n>7; }).pop();
-
     return req.db.find({num: r._id},{sort: {source_file_idx:1}}).toArray()
-    .then(function(lines){
-      var hdr = lines[0];
-      hdr.subtotal = lines.reduce(function(t,r){ return t+r.amount; },0)
-      hdr.lines = lines;
-      console.log('hdr',hdr);
-      return hdr;
-    })
+		.then(procInvoice)
     .catch(function(err){ throw err; })
   });
 }
+
+
+function byNum(req,num){
+	return req.db.find({num: num},{sort: {source_file_idx:1}})
+	.toArray()
+	.then(procInvoice)
+  .catch(function(err){ throw err; })
+}
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   console.log("got to / route");
   return absorb(req)
   .then(function(inv){
-    console.log(inv);
+    //console.log(inv);
     res.render('index', inv);
   })
   .catch(function(err){
     console.log(err);
   })
 });
+
+
+/* GET home page. */
+router.get('/inv/:num', function(req, res, next) {
+  console.log("got to /inv:num route");
+  return byNum(req,req.params.num)
+  .then(function(inv){
+    res.render('index', inv);
+  })
+  .catch(function(err){
+    console.log(err);
+  })
+});
+
 
 module.exports = router;
